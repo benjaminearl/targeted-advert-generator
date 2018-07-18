a697a808b83b02577ac83d8967dd5287b8635bbd
benjaminearl <bnjmnearl@gmail.com>
2018-07-18 17:56:40 +0200

    Update server.js

--- interface/server.js
+++ interface/server.js
@@ -32,7 +32,7 @@ function getFiles() {
 
 
 app.get('/', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
