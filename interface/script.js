95596374e0274826423b1ac25e3ded4f436f716a
Benjamin Earl <bnjmnearl@gmail.com>
2018-07-18 16:44:40 +0200

    delete http

--- interface/script.js
+++ interface/script.js
@@ -1,11 +1,3 @@
-#!/usr/bin/env nodejs
-
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, 'localhost');
-
 document.addEventListener('DOMContentLoaded', function(){
 	appendControls();
 	getModels();
